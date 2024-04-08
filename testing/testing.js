261bdddd2617c8d0aaa699a2ed6eb10c20e5cf71
Ankit kumar <155428887+ankitsarwal7@users.noreply.github.com>
2024-04-08 12:06:18 +0000

    dom larning

---
+++ testing/testing.js
@@ -0,0 +1,137 @@
+// const accountid = 1628138
+// let accountemail = "ankitsarwal@gmail.com"
+// let password = "2836823aks"
+
+
+// console.log(accountid);
+
+// console.table([accountemail,accountid,password])
+
+// console.log(typeof password);
+
+
+// ****************************    iifi    *******************************
+
+
+// (function test(){
+//     console.log(`DB CONNECTED`);
+// })();
+
+// ((name) => {
+//     console.log(`DB CONNECTED TWO ${name}`);
+// })('ankit')
+
+
+
+
+// const user = {
+// username: "sam",
+// password: 12234,
+
+// welcomeMessage: function(){
+
+// console.log(`${this.username} , welcome to my channel`);
+
+// }
+
+// }
+
+
+
+
+
+// const sam = () =>{
+//     let username = "ankit"
+//     console.log(typeof this);
+// }
+
+
+// const usetloggedin = true
+// const temrature = 48
+
+// if (temrature==40) {
+//     console.log('less than 50');
+    
+// } else {
+//     console.log("tem is greater than 50");
+// }
+
+
+// for (let i = 0; i <= 20 i++) {
+//     const element = i;
+// if (element == 10) {
+//     console.log("10 is lower");
+    
+// }   
+// console.log(element);
+// }
+
+
+// let number= [1, 2, 3, 4,];
+// for (let i = 0; i < number.length; i++) {
+// console.log(number[i]);
+// }
+
+// for (let i = 0; i <=10; i++) {
+    
+//     for (let j = 0; j <=10; j++) {
+//         console.log(`inner loop ${i}   outerloop ${j}`);
+//         console.log(i +   '*'  + j + '=' + i * j  );
+        
+//     }
+// }
+
+
+
+
+
+
+
+
+
+// // Define the number of rows for the pattern
+// const numberOfRows = 5;
+
+// // Outer loop for the number of rows
+// for (let i = 1; i <= numberOfRows; i++) {
+//     let row = '';
+
+//     // Inner loop for printing stars in each row
+//     for (let j = 1; j <= i; j++) {
+//         row += '* ';
+//     }
+
+//     // Print the row
+//     console.log(row);
+// }
+
+
+
+
+
+
+
+
+// for (let i = 1; i <= 10; i++) {
+//     //console.log(`Outer loop value: ${i}`);
+//    for (let j = 1; j <= 10; j++) {
+//     console.log(`Inner loop value ${j} and inner loop ${i}`);
+//     console.log(i + '*' + j + ' = ' + i*j );
+//    }
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
